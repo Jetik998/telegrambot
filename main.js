db87c6c31139bc5f7ff68e7d0d6b4f2d7822b045
Jetik998 <l-lellboy@yandex.ru>
2025-01-08 20:29:59 +0400

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import { Telegraf, Markup } from 'telegraf'
 const token = '8149375289:AAFBGe0TSYQ_xpXPToZQgX8vl3yeEYTPcBw'
 const webAppUrl = 'https://whiteclock.ru'
 
+// Инициализация бота
 const bot = new Telegraf(token)
 
+// Команда /start
 bot.command('start', (ctx) => {
   ctx.reply(
     'Добро пожаловать! Нажмите на кнопку ниже, чтобы запустить приложение 🚀',
@@ -22,11 +24,20 @@ bot.command('start', (ctx) => {
 
 // Обработка нажатия на кнопки callback
 bot.action('help', (ctx) => {
-  ctx.reply('Напишите нам, если нужна помощь! 📧');
-});
+  ctx.reply('Напишите нам, если нужна помощь! 📧')
+})
 
 bot.action('contacts', (ctx) => {
-  ctx.reply('Наши контакты: email@domain.com 📱');
-});
+  ctx.reply('Наши контакты: email@domain.com 📱')
+})
 
-bot.launch()
+// Запуск бота, привязка к порту (для платформ, как Render)
+const port = process.env.PORT || 3000
+bot.launch({
+  webhook: {
+    domain: 'https://telegrambot-xhbu.onrender.com',  // Замените на ваш URL на Render
+    port: port
+  }
+}).then(() => {
+  console.log('Bot is running...');
+})
